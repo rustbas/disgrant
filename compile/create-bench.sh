49a1627c21dfab59fe889e01fd62332ba7e53150
Basyrov Rustam <hrustbas@gmail.com>
2024-11-08 13:35:57 +0300

    make bench script

--- compile/create-bench.sh
+++ compile/create-bench.sh
@@ -3,8 +3,11 @@
 # set -xe
 
 N=2
+SRC_DIR="src"
+OBJ_DIR="obj"
+MAKEFILE="makefile"
 
-cat > main.c << EOF
+cat > $SRC_DIR/main.c << EOF
 #include <stdio.h>
 
 int main() {
@@ -14,9 +17,43 @@ int main() {
 }
 EOF
 
-mkdir --parents src 
+mkdir --parents $SRC_DIR
 
 for i in $(seq $N)
 do
-    touch src/lib$i.c
+    file="$SRC_DIR/lib$i.c"
+
+    cat > $file << EOF
+    int add$i(int x, int y) {
+        return x + y;
+    }
+EOF
 done
+
+mkdir --parents $OBJ_DIR
+
+echo -n "all:" > $MAKEFILE
+
+for i in $(seq $N)
+do
+    echo -n " $OBJ_DIR/lib$i.o" >> $MAKEFILE
+done
+
+echo "" >> $MAKEFILE
+
+echo "\tgcc $SRC_DIR/main.c $OBJ_DIR/*.o -o res.o" >> $MAKEFILE
+
+
+for i in $(seq $N)
+do
+    src_file="$SRC_DIR/lib$i.c"
+    obj_file="$OBJ_DIR/lib$i.o"
+
+    echo "$obj_file:" >> $MAKEFILE
+    echo "\tgcc $src_file --shared -o $obj_file" >> $MAKEFILE
+    echo "\tsleep 5" >> $MAKEFILE
+
+done
+
+echo "clean:" >> $MAKEFILE
+echo "\trm obj/*" >> $MAKEFILE
